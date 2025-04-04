fa105a739d05ab99be02378b0e4cc9a88e5264bf
Chao Dang <eric97irl@gmail.com>
2025-04-04 17:59:32 +0100

    update configs

--- .functions.d/zpush.zsh
+++ .functions.d/zpush.zsh
@@ -1,4 +1,13 @@
-# 🚀 Git 提交并推送（支持参数或默认）
+zpush() {
+  cd ~/sre-terminal
+  git stash push -m "🔒 zpush auto stash"
+  git pull --rebase
+  git stash pop
+  git add .
+  git commit -m "${1:-update configs}"
+  git push
+  echo "✅ zpush 完成"
+}# 🚀 Git 提交并推送（支持参数或默认）
 zpush() {
   cd ~/sre-terminal
   git add .
